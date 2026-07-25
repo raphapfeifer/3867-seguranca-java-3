39758d543b752addfd578fc6add959f9d2e5a38a
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-07-25 00:09:00 -0300

    Custom filter

---
+++ src/main/java/br/com/forum_hub/infra/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.forum_hub.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder encripter(){
+        return new BCryptPasswordEncoder();
+    }
+}
